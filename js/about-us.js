796929ab1331a82cb38c0a7faa46c3f224abea54
lucifer's lab <zaidamreliya002@gmail.com>
2025-09-01 13:24:24 +0530

    upload about-us page

---
+++ js/about-us.js
@@ -0,0 +1,4 @@
+document.getElementById('bookNowBtn').addEventListener('click', function() {
+    alert('Thank you for choosing Destinova! Redirecting to the booking page...');
+    // Here, redirection or other booking actions could be added
+});
